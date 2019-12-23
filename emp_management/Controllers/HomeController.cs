dad3a3a977c72d9448935d0ac56a86fdb2c97db7
atipchao <atip.chaogunhar@cookcountyil.gov>
2019-12-22 18:22:38 -0600

    Vid# 53 File Upload Wheep!

--- emp_management/Controllers/HomeController.cs
+++ emp_management/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using emp_management.Models;
 using emp_management.ViewModes;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,32 +12,34 @@ namespace emp_management.Controllers
 {
     public class HomeController : Controller
     {
-        private IEmployeeRepository _employeeRepository;
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IHostingEnvironment _hostingEnvironment;
 
-        public HomeController(IEmployeeRepository employeeRepository)
+        public HomeController(IEmployeeRepository employeeRepository, IHostingEnvironment hostingEnvironment)
         {
             _employeeRepository = employeeRepository;
+            _hostingEnvironment = hostingEnvironment;
         }
 
         //public JsonResult Index()
-        
+
         public ViewResult Index()
         {
             IEnumerable<Employee> employees;
             employees = _employeeRepository.GetAllEmployee();
             return View(employees);
         }
-        
+
         public ViewResult Details(int? id)
         {
             Employee em = new Employee();
 
-            em = _employeeRepository.GetEmployee(id??1);
+            em = _employeeRepository.GetEmployee(id ?? 1);
             HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
             {
 
                 Employee = em,
-                PageTitle = "Emp Details"               
+                PageTitle = "Emp Details"
 
             };
 
@@ -49,13 +53,33 @@ namespace emp_management.Controllers
 
         [HttpPost]
         //public RedirectToActionResult Create(Employee emp)
-        public ActionResult Create(Employee emp)
+        public ActionResult Create(EmpCreateViweModel emp)
         {
             if (ModelState.IsValid)
             {
-                Employee newEmp;
-                newEmp = _employeeRepository.Add(emp);
-                //return View(newEmp);
+                string uniqueFilename = null;
+                if (emp.Photo != null)
+                {
+                    string uploadFolder = Path.Combine(_hostingEnvironment.WebRootPath, "images");
+                    uniqueFilename = Guid.NewGuid().ToString() + "_" + emp.Photo.FileName;
+                    string filePath = Path.Combine(uploadFolder, uniqueFilename);
+                    emp.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
+
+                }
+                Employee newEmp = new Employee
+                {
+                    Name = emp.Name,
+                    Email = emp.Email,
+                    Department = emp.Department,
+                    PhotoPath = uniqueFilename
+                };
+                try
+                {
+                    newEmp = _employeeRepository.Add(newEmp);
+                }catch(Exception e)
+                {
+                    Response.Redirect("error.html");
+                }
                 return RedirectToAction("details", new { id = newEmp.Id });
             }
             else
@@ -63,8 +87,8 @@ namespace emp_management.Controllers
                 //return RedirectToAction("create", emp);
                 return View();
             }
-                
-            
+
+
         }
     }
 }
