7f50afeb0d9d52936dad011f1e6e3eb2c67c085a
atipchao <atip.chaogunhar@cookcountyil.gov>
2020-01-05 14:40:54 -0600

    #80 Implement EditRoles() in Administration controller

--- emp_management/Controllers/AdministrationController.cs
+++ emp_management/Controllers/AdministrationController.cs
@@ -1,4 +1,5 @@
-﻿using emp_management.ViewModes;
+﻿using emp_management.Models;
+using emp_management.ViewModes;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -11,10 +12,13 @@ namespace emp_management.Controllers
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        public AdministrationController(RoleManager<IdentityRole> roleManager,
+                                        UserManager<ApplicationUser> userManager)
         {
             this._roleManager = roleManager;
+            this._userManager = userManager;
         }
 
         [HttpGet]
@@ -51,5 +55,69 @@ namespace emp_management.Controllers
             var roles = _roleManager.Roles; 
             return View(roles);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            // uses roleId to get role from RoleManagerService 
+            var role =  await _roleManager.FindByIdAsync(id);
+            if(role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found..";
+                return View("NotFound");
+            }
+            else
+            {
+
+            }
+            var model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name
+            };
+            //Get uses within the role by using IdentityUser Service
+            foreach(var user in  _userManager.Users)
+            {
+                if (await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+
+            if(ModelState.IsValid)
+            {
+
+            }
+            // uses roleId to get role from RoleManagerService 
+            var role = await _roleManager.FindByIdAsync(model.Id);
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {model.Id} cannot be found..";
+                return View("NotFound");
+            }
+            else
+            {
+                role.Name = model.RoleName;
+                // Now we update the role using "roleManager.UpdateAsync"  ==> this returns IdentityResult
+                var result = await _roleManager.UpdateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+                // If there's any errro
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View(model);
+            }
+        }
+
     }
 }
---
+++ emp_management/ViewModes/EditRoleViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace emp_management.ViewModes
+{
+    public class EditRoleViewModel
+    {
+        public EditRoleViewModel()
+        {
+            Users = new List<string>(); // here we initialize the Users 
+        }
+        public string Id { get; set; }
+        [Required(ErrorMessage = "Role name is required..")]
+        public string RoleName { get; set; }
+        public List<string> Users { get; set; }  
+    }
+}
