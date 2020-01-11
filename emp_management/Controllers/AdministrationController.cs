7896f12bef54856728a398d22a599b5153d7fc04
atipchao <atip.chaogunhar@cookcountyil.gov>
2020-01-11 11:33:59 -0600

    #82 Add authorize role

--- emp_management/Controllers/AdministrationController.cs
+++ emp_management/Controllers/AdministrationController.cs
@@ -1,5 +1,6 @@
 ﻿using emp_management.Models;
 using emp_management.ViewModes;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace emp_management.Controllers
 {
+    [Authorize(Roles = "admin")]
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
